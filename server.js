aeac76d34dea3d422cfc8e0cd185a8bce12a6177
Poly Goveia <poly.goveia@gmail.com>
2019-08-30 19:09:28 -0700

    get initial server working

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const server = express();
+server.use(express.json());
+
+server.use(logger);
+
+// const projectsRoute = require("./routes/projectsRoutes");
+// server.use("/projects", projectsRoute);
+
+// const resourceRoute = require("./routes/resourceRoutes");
+// server.use("/resources", resourceRoute);
+
+// const tasksRoute = require("./routes/tasksRoute");
+// server.use("/tasks", tasksRoute);
+
+server.get("/", (req, res) => {
+  res.send(`Working!`);
+});
+
+function logger(req, res, next) {
+  console.log(
+    `Method: ${req.method}, url: ${
+      req.url
+    }, timestamp: [${new Date().toISOString()}]`
+  );
+  next();
+}
+
+module.exports = server;
